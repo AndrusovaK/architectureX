88a7019558ff81d509d263e6423ebbcbe855925f
AndrusovaK <andrusova_ksenia@mail.ru>
2015-11-19 23:36:42 +0300

    first commit
    есть много доработок

---
+++ js/sliders-initiate.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  			$('.client-carousel').slick({
+    			autoplay: true,
+				autoplaySpeed: 2000,
+				prevArrow: $('.client-prev'),
+				nextArrow: $('.client-next'),
+				slidesToShow: 5,
+				slidesToScroll: 1,
+				
+  			});
+		});
+
+
+
+$(document).ready(function(){
+  			$('.reference-carousel').slick({
+				prevArrow: $('.ref-prev'),
+				nextArrow: $('.ref-next'),
+  			});
+		});
