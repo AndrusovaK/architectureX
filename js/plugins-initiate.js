a19f40e0945c186cba33a4600ddfab23c1f552ac
AndrusovaK <andrusova_ksenia@mail.ru>
2015-11-25 23:03:29 +0300

    Табы вариант 2. Лучше!

--- js/plugins-initiate.js
+++ js/plugins-initiate.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+
+  $('.tabs a').on('click',function(e){
+    e.preventDefault();
+    $(this).closest('.tabs').find('.tabs__item').removeClass('tabs__item--active');
+    $(this).closest('.tabs__item').addClass('tabs__item--active');
+    
+    var tabs_cnt_id = $(this).closest('.tabs').data('tabs-content-id');
+    $(tabs_cnt_id).find('.tabs-content__item').removeClass('tabs-content__item--active');
+    $(this.hash).addClass('tabs-content__item--active');
+  });
+  
+});
+
+
+$(document).ready(function(){
   			$('.client-carousel').slick({
     			autoplay: true,
 				autoplaySpeed: 2000,
